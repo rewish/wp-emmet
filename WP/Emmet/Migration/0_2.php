e940d6969462070b8d1151b254ac529135108280
rewish <rewish.org@gmail.com>
2013-07-05 13:23:14 +0900

    Fix warnings for migration version 0.2

--- WP/Emmet/Migration/0_2.php
+++ WP/Emmet/Migration/0_2.php
@@ -2,11 +2,17 @@
 class WP_Emmet_Migration_0_2 {
 	public static function migrate(WP_Emmet $context) {
 		$options = $context->Options->get();
+		$updated = false;
 
 		foreach (array('options', 'variables') as $key) {
-			$options['textarea'][$key] = $options[$key];
+			if (isset($options[$key])) {
+				$options['textarea'][$key] = $options[$key];
+				$updated = true;
+			}
 		}
 
-		$context->Options->save($options);
+		if ($updated) {
+			$context->Options->save($options);
+		}
 	}
 }

0ef6058754fa03006be460db08405db483971482
rewish <rewish.org@gmail.com>
2013-07-10 02:55:44 +0900

    Refactor

--- WP/Emmet.php
+++ WP/Emmet.php
@@ -80,22 +80,15 @@ class WP_Emmet {
 		$this->Options = new WP_Emmet_Options();
 		$this->CodeMirror = new WP_Emmet_CodeMirror();
 
-		$this->migrate();
-
-		$this->setupActions();
-	}
-
-	/**
-	 * Migrate
-	 */
-	public function migrate() {
 		WP_Emmet_Migration::migrate($this);
+
+		$this->registerHooks();
 	}
 
 	/**
-	 * Setup actions
+	 * Register hooks
 	 */
-	public function setupActions() {
+	public function registerHooks() {
 		add_action('admin_print_styles', array($this, 'printStyles'));
 		add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
 		add_action('admin_print_footer_scripts', array($this, 'applyScripts'), 1);

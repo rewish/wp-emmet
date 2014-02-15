22b3e8dca2ac076095d73e9192f635744221eb22
rewish <rewish.org@gmail.com>
2014-02-15 22:58:47 +0900

    Move the Fake call to langs directory

---
+++ langs/fake.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Fake call for the gettext
+ */
+__('Expand Abbreviation');
+__('Match Pair Outward');
+__('Match Pair Inward');
+__('Wrap with Abbreviation');
+__('Next Edit Point');
+__('Prev Edit Point');
+__('Select Line');
+__('Merge Lines');
+__('Toggle Comment');
+__('Split/Join Tag');
+__('Remove Tag');
+__('Evaluate Math Expression');
+__('Increment number by 1');
+__('Decrement number by 1');
+__('Increment number by 0.1');
+__('Decrement number by 0.1');
+__('Increment number by 10');
+__('Decrement number by 10');
+__('Select Next Item');
+__('Select Previous Item');
+__('Reflect CSS Value');
--- views/options.php
+++ views/options.php
@@ -165,28 +165,3 @@ jQuery(function($) {
 	});
 });
 </script>
-
-<?php
-// Fake call for gettext
-__('Expand Abbreviation');
-__('Match Pair Outward');
-__('Match Pair Inward');
-__('Wrap with Abbreviation');
-__('Next Edit Point');
-__('Prev Edit Point');
-__('Select Line');
-__('Merge Lines');
-__('Toggle Comment');
-__('Split/Join Tag');
-__('Remove Tag');
-__('Evaluate Math Expression');
-__('Increment number by 1');
-__('Decrement number by 1');
-__('Increment number by 0.1');
-__('Decrement number by 0.1');
-__('Increment number by 10');
-__('Decrement number by 10');
-__('Select Next Item');
-__('Select Previous Item');
-__('Reflect CSS Value');
-?>
